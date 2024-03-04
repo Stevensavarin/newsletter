2bad952d5f85c2e3a9acc940bff716cd0cf5776e
steven_savarin <srssdesing@gmail.com>
2024-03-04 19:56:26 +0100

    landing page and auth config

---
+++ src/modules/home/home.tsx
@@ -0,0 +1,25 @@
+import Header from '@/shared/widgets/header/header'
+import React from 'react'
+import Banner from './elements/banner'
+import Branding from "@/modules/home/elements/branding"
+import Benefits from "@/modules/home/elements/benefits"
+import FeatureHighlight from "@/modules/home/elements/feature.highlight"
+import Pricing from "@/modules/home/elements/pricing"
+import Footer from "@/shared/widgets/footer/footer";
+
+
+const Home = () => {
+  return (
+    <div>
+        <Header />
+        <Banner />
+        <Branding />
+        <Benefits />
+        <FeatureHighlight />
+        <Pricing />
+        <Footer />
+    </div>
+  )
+}
+
+export default Home
---
+++ src/shared/utils/Providers.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { NextUIProvider } from "@nextui-org/react";
+import { usePathname } from "next/navigation";
+import { useUser } from "@clerk/nextjs";
+
+
+interface ProviderProps {
+    children: React.ReactNode;
+}
+
+export default function Providers({ children }: ProviderProps) {
+    const pathname = usePathname();
+
+    const { isLoaded, user } = useUser();
+
+    if (!isLoaded) {
+        return null;
+    }
+
+    return (
+        <NextUIProvider>
+      {pathname !== "/dashboard/new-email" &&
+      pathname !== "/" &&
+      pathname !== "/sign-up" &&
+      pathname !== "/subscribe" &&
+      pathname !== "/success" &&
+      pathname !== "/sign-in" ? (
+        <div className="w-full flex">
+          <div className="w-[290px] h-screen overflow-y-scroll">
+        </div>
+          {children}
+        </div>
+      ) : (
+        <>{children}</>
+      )}
+      
+    </NextUIProvider>
+    );
+}
+
